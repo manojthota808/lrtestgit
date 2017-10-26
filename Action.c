37f9b05acdd350e7d5cfacd5cdd217d8328d9ab9
unknown <a_loadrunner@GRPDOM>
2017-10-26 10:35:22 +0100

    testcommit3

--- Action.c
+++ Action.c
@@ -15,6 +15,15 @@ Action()
 	
 //	testtttt
 	
+		web_custom_request("web_custom_request",
+		"URL=http://www.google.com/",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+	
 	
 	
 	
